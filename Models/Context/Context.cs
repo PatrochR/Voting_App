40d439c7e47c38ac61c252f123ed6bae6e75796d
Patroch <88762286+PatrochR@users.noreply.github.com>
2022-08-13 06:58:45 +0430

    Context

--- Models/Context/Context.cs
+++ Models/Context/Context.cs
@@ -1,17 +1,17 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Voting_App.Models;
 
 
 namespace Models.Context {
-    public class Context : IdentityDbContext<ApplicationUser> {
+    public class Context : DbContext {
         public Context (DbContextOptions options) : base (options) 
         {
 
         }
         
         public DbSet<Vote> Votes { get; set; }
-
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<User> Users { get; set; }
         
     }
 }
