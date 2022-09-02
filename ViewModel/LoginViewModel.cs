ba681ecd48129d7433e0fcf72835be8cfb321a96
Patroch <88762286+PatrochR@users.noreply.github.com>
2022-09-02 08:44:52 +0430

    Change ViewModels

--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -4,9 +4,12 @@ namespace Voting_App.ViewModel
     public class LoginViewModel
     {
         [Required]
+        [Display(Name = "نام کاربری", AutoGenerateFilter=false)]  
+
         public string UserName { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "کلمه عبور", AutoGenerateFilter=false)]  
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
--- ViewModel/RegisterViewModel.cs
+++ ViewModel/RegisterViewModel.cs
@@ -5,9 +5,11 @@ namespace Voting_App.ViewModel
     {
         [Required]
         [MaxLength(50)]
+        [Display(Name = "نام کابری", AutoGenerateFilter=false)]  
         public string UserName { get; set; }
         [Required]
         [MaxLength(50)]
+        [Display(Name = "کلمه عبور", AutoGenerateFilter=false)]  
         public string Password { get; set; }
     }
 }
